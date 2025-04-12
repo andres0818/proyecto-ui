2521698b63537777f5687c0e373455db9c7ff1d7
andres0818 <c.andres.perez.h@gmail.com>
2025-04-12 09:27:53 -0600

    Se organiza la base del perfil del usuario

---
+++ js/perfilUsuario.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+    Init()
+});
+
+function Init(){
+    document.querySelector('.toggle-button').addEventListener("click", function() {
+  
+        document.querySelector('.left-sidebar').classList.toggle('minimize');
+      });
+      
+      document.querySelector('.user-profile').addEventListener('click', function() {
+        
+        document.querySelector('.left-sidebar').classList.toggle('minimize');
+      });
+
+}
